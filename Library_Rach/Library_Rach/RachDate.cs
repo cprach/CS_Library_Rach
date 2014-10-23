dddf6577fd26b99e0042144543f5d5b3632be53c
unknown <rcolley@MELSW021.CPAU>
2014-10-24 10:25:00 +1100

    MOAR

---
+++ Library_Rach/Library_Rach/RachDate.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library_Rach
+{
+    public class RachDate
+    {
+
+        public String addHoursToUTCDate(int offset)
+        {
+            return DateTimeOffset.UtcNow.AddHours(10).ToString("dd/MM/yyyy");
+        }
+        public String addHoursToUTCTime(int offset)
+        {
+            return DateTimeOffset.UtcNow.AddHours(10).ToString("h:mm tt");
+        }
+
+    }
+
+
+
+
+}
